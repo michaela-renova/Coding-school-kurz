8f9a3a12a29a0b7617c66cf0907f340eb29e9336
michaela-renova <renovamichaela@gmail.com>
2025-10-24 18:24:35 +0200

    hotový úkol

--- ukol.php
+++ ukol.php
@@ -10,7 +10,7 @@ echo "$name, $age, $height";
 //2
 $firstName = "Michaela";
 $lastName = "Renová";
-$fullName = $firstName . "" . $lastName;
+$fullName = $firstName . " " . $lastName;
 echo $fullName;
 
 //3
@@ -20,15 +20,16 @@ echo gettype($x);
 //4
 $number = "42";
 $sum = (int)$number + 8;
+echo $sum;
 
 //5
 $colors = ["red", "green", "blue",];
-echo "$colors[1]";
+echo $colors[1];
 
 //2 podmínky a logika
 //6
-$score = "70";
-if ($score >90) {
+$score = 70;
+if ($score > 90) {
     echo "Výborně";
 } elseif ($score >= 60 && $score <= 89) {
     echo "Dobře";
@@ -76,13 +77,15 @@ function add($a, $b) {
 function isEven($num) {
     if ($num % 2 == 0){
     return true;
+    }else{
+    return false;
 }
 }
 
 //14
-$numbers = [18, 65, 11,];
+$numbers = [18, 65, 11, 12, 38, 3,];
 function average($numbers) {
-    return array_sum();
+    return array_sum($numbers) / count($numbers);
 }
 
 //15 samohlásky
@@ -91,8 +94,8 @@ $string = "nejneobhospodařovávatelnější";
 function countVowels ($string) {
     $count = 0;
     $vowels = ["a", "e", "i", "o", "u", "y", "á", "é", "í", "ó", "ú", "ů", "ý", "A", "E", "I", "O", "U", "Y", "Á", "É", "Í", "Ó", "Ú", "Ů", "Ý",];
-
-    $letter = str_split($string);
+    //pro znaky s diakritikou je lepší využívat verzi s předponou mb
+    $letter = mb_str_split($string);
 
     foreach ($letter as $letter){
        if (in_array($letter, $vowels)){
@@ -106,33 +109,38 @@ function countVowels ($string) {
 //16
 $text = "ahoj";
 $reversedText = strrev($text);
+echo $reversedText;
 
 //17
 $word = "php";
 $upperCaseWord = ucfirst($word);
+echo $upperCaseWord;
 
 //18
 $sentence = "Laravel je PHP framework.";
 $countA = substr_count($sentence, "a");
+echo $countA;
 
 //19
 $containsLaravel = str_contains($sentence, "Laravel");
+echo $containsLaravel ? "Obsahuje Laravel" : "Neobsahuje Laravel";
 
 //20
 $replaceEwith3= str_replace("e", "3", $sentence);
+echo $replaceEwith3;
 
 //5 práce s poli a logikou
 //21
-$numbers2 = [1, 2, 3, 4, 5];
+$numbersA = [1, 2, 3, 4, 5,];
 
-$doubledNumbers2 = array_map(function($num) {
-    return $num * 2;
-}, $numbers2);
+$doubledNumbersA = array_map(function($numA) {
+    return $numA * 2;
+}, $numbersA);
 
-print_r($doubledNumbers2);
+print_r($doubledNumbersA);
 
 //22
-$names = ["Eva", "Jan", "Eva", "Petr"];
+$names = ["Eva", "Jan", "Eva", "Petr",];
 $noDuplicateNames = array_unique($names);
 
 //23
@@ -146,8 +154,10 @@ if (in_array("Jan", $names)) {
 }
 
 //25
-$numbers3 = [9, 2, 5, 1,];
-$sortedNumbers3 = sort($numbers3);
+//funkce sort vrací bool
+$numbers = [9, 2, 5, 1,];
+sort($numbers);
+print_r($numbers);
 
 //26
 $words = ["monitor", "klávesnice", "myš",];
@@ -164,17 +174,20 @@ echo findLongestWord($words);
 
 //27
 function sumEvenNumbers($numbers) {
-    $sum2 = 0;
-    foreach ($numbers as $n) {
-        if ($n % 2 == 0) {
-            $sum2 += $n;
+    $sumEvenNum = 0;
+    foreach ($numbers as $num) {
+        if ($num % 2 == 0) {
+            $sumEvenNum += $num;
         }
     }
-    return $sum2;
+    return $sumEvenNum;
 }
+
+echo sumEvenNumbers($numbers);
 
 //28
 //array_reversed nefunguje na řetězec
+$sentence = "Laravel je PHP framework.";
 function reverseWords($sentence) {
     //rozdělení na slova
     $words = explode(" ", $sentence);
@@ -189,10 +202,21 @@ $array = [15, 19, 15, 5,];
 function hasDuplicates($array){
     if (count($array) !== count(array_unique($array))){
         return true;
+           } else{
+            return false;
            }
 }
+echo hasDuplicates($array);
 
 //30
+$stringB = "jablko";
+function uniqueLetters($stringB) {
+    $letterB = mb_str_split($stringB);
+    $unique = array_unique($letterB);
+    return array_values($unique);
+}
+
+print_r (uniqueLetters($stringB));
 
 
 
